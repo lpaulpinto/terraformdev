6e154a22ab1d398211b843179c3e02696e8591d1
lpaulpinto <paulpinto@live.com>
2021-05-05 22:46:22 -0500

    Terraform

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # terraformdev
+
+#PaulPinto
+{
+"appId": "2f3e5bfd-eec4-4e9f-b927-c8081089d9e8",
+"displayName": "azure-cli-2021-05-06-03-07-51",
+"name": "http://azure-cli-2021-05-06-03-07-51",
+"password": "gHcNZ60~6Xbw.AEU-riTAViXpmVDN0BkIK",
+"tenant": "0e2f4bf8-94ce-42b9-8a99-cb75a38402f7"
+}
---
+++ azure-terraform/kubernetes/variables.tf
@@ -0,0 +1,27 @@
+variable client_id {}
+variable client_secret {}
+variable ssh_public_key {}
+
+variable environment {
+    default = "dev"
+}
+
+variable location {
+    default = "westeurope"
+}
+
+variable node_count {
+  default = 2
+}
+
+variable dns_prefix {
+  default = "k8sAforo255lppp"
+}
+
+variable cluster_name {
+  default = "k8sAforo255lppp"
+}
+
+variable resource_group {
+  default = "kubernetesAforo255"
+}
